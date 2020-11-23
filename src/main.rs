290d6e7393346364bd3d79dba06f6ef5e4dea4bd
John Shewchuk <johnshew@users.noreply.github.com>
2020-11-23 10:45:26 -0500

    dynamic graph bars

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,36 @@ extern crate native_windows_gui as nwg; // Optional. Only if the derive macro is
 use nwd::{NwgPartial, NwgUi};
 use nwg::NativeUi;
 
+pub struct AppData {
+    count: u32,
+    total: u32,
+    min: u32,
+    max: u32,
+    probes: VecDeque<(u128, u32)>,
+}
+
+impl Default for AppData {
+    fn default() -> Self {
+        AppData {
+            count: 0,
+            total: 0,
+            min: u32::MAX,
+            max: 0,
+            probes: VecDeque::new(),
+        }
+    }
+}
+
+impl AppData {
+    fn average(&self) -> f32 {
+        return self.total as f32 / self.count as f32;
+    }
+    // fn intervals(&self,  start: u128, end: u128) {}
+}
+
 #[derive(Default, NwgUi)]
 pub struct BasicApp {
-    data: RefCell<MyData>,
+    data: RefCell<AppData>,
 
     #[nwg_control(size: (300, 135), position: (300, 300), title: "Basic example", flags: "MAIN_WINDOW|VISIBLE")]
     #[nwg_events( OnWindowClose: [BasicApp::say_goodbye], OnInit: [BasicApp::on_init] )]
@@ -66,6 +93,9 @@ impl BasicApp {
     fn on_init(&self) {
         self.slider.set_range_min(0);
         self.slider.set_range_max(100);
+        self.graph
+            .set_values(0, 200, vec![(10, (8, 20)), (12, (50, 200))]);
+        self.graph.set_bars_len(2);
     }
 
     fn _say_hello(&self) {
@@ -83,10 +113,6 @@ impl BasicApp {
         data.min = u32::MAX;
         data.max = 0;
         data.probes.clear();
-
-        // self.graph1.set_state(nwg::ProgressBarState::Normal);
-        // self.graph1.set_pos(50);
-        // self.graph1.set_state(nwg::ProgressBarState::Paused);
     }
 
     fn say_goodbye(&self) {
@@ -183,65 +209,99 @@ impl Default for GraphData {
 
 #[derive(Default, NwgPartial)]
 pub struct GraphUi {
+    data: RefCell<GraphData>,
 
-    #[nwg_layout(spacing: 1)]
+    #[nwg_layout( spacing: 1)]
     grid: nwg::GridLayout,
 
     #[nwg_control(flags: "NONE")]
-    #[nwg_layout_item(layout: grid, col:0, row: 0)]
+    #[nwg_layout_item(layout: grid, row: 0, col: 0 )]
     #[nwg_events( OnResize: [GraphUi::on_resize])]
     frame: nwg::Frame,
 
-    #[nwg_control(position: (10,10), size: (100,100), background_color: Some([0, 255, 255]))]
-    frame1: nwg::ImageFrame,
+    // #[nwg_control(position: (10,10), size: (100,100), background_color: Some([0, 255, 255]))]
+    // frame1: nwg::ImageFrame,
 
-    #[nwg_control(position: (110,10), size: (100,100), background_color: Some([255, 0, 255]))]
-    frame2: nwg::ImageFrame,
-
-    _bars: RefCell<Vec<nwg::Frame>>,
+    // #[nwg_control(position: (110,10), size: (100,100), background_color: Some([255, 0, 255]))]
+    // frame2: nwg::ImageFrame,
+    bars: RefCell<Vec<nwg::ImageFrame>>,
 }
 
 impl GraphUi {
-    fn _set_values(&self, _bars: Vec<(u16, (u16, u16))>) {}
+    fn set_values(&self, min: u16, max: u16, bars: Vec<(u16, (u16, u16))>) {
+        let mut data = self.data.borrow_mut();
+        data.min = min;
+        data.max = max;
+        data.bars = bars.to_vec();
+    }
+
+    fn add_bar(&self, column: u32) {
+        let mut new_bar = Default::default();
+        nwg::ImageFrame::builder()
+            .parent(&self.frame)
+            .background_color(Some([0, 255, 255]))
+            .build(&mut new_bar)
+            .expect("Failed to build button");
+
+        // self.grid.add_child(0, column, &new_bar);
+
+        let mut graph_bars = self.bars.borrow_mut();
+        graph_bars.push(new_bar);
+    }
+
+    fn set_bars_len(&self, len: usize) {
+        let mut graph_bars_len = 0;
+        { 
+            graph_bars_len = self.bars.borrow().len();
+        }
+        if len > graph_bars_len {
+            for i in graph_bars_len..len {
+                let mut new_bar = Default::default();
+                nwg::ImageFrame::builder()
+                    .parent(&self.frame)
+                    .background_color(Some([0, 255, 255]))
+                    .build(&mut new_bar)
+                    .expect("Failed to build button");
+        
+                // self.grid.add_child(0, column, &new_bar);
+        
+                let mut graph_bars = self.bars.borrow_mut();
+                graph_bars.push(new_bar);
+            }
+        }
+
+        // self.on_resize();
+    }
 
     fn on_resize(&self) {
         self.frame.set_visible(true);
-        
+
         let (w, h) = self.frame.size();
         let (l, t) = self.frame.position();
-        self.frame1.set_size(w / 2, h);
-        self.frame1.set_position(l,t);
 
-        self.frame2.set_size(w/2, h/3);
-        self.frame2.set_position(((w / 2) as i32) + l, t + (h/3) as i32);
-    }
-}
-
-pub struct MyData {
-    count: u32,
-    total: u32,
-    min: u32,
-    max: u32,
-    probes: VecDeque<(u128, u32)>,
-}
-
-impl Default for MyData {
-    fn default() -> Self {
-        MyData {
-            count: 0,
-            total: 0,
-            min: u32::MAX,
-            max: 0,
-            probes: VecDeque::new(),
+        let mut data_len = 0;
+        {
+            data_len = self.data.borrow().bars.len();            
+        }
+        for i in 0..data_len {
+            let data = self.data.borrow();
+            let bar = data.bars[i];
+            let (_pos, (low, high)) = bar;
+            let bar_h_ratio = (high - low) as f32 / (data.max - data.min) as f32;
+            let bar_h = (h as f32 * bar_h_ratio) as u32;
+            let gap_ratio = (data.max - high) as f32 / (data.max - data.min) as f32;
+            let gap_h = (h as f32 * gap_ratio) as i32;
+            {
+                let  bars = self.bars.borrow();
+                bars.get(i).unwrap().set_size(w / data_len as u32, bar_h);
+                bars.get(i).unwrap().set_position(
+                    (w / data_len as u32 * i as u32) as i32 + l,
+                    gap_h + t,
+                );
+                
+            }
         }
     }
-}
-
-impl MyData {
-    fn average(&self) -> f32 {
-        return self.total as f32 / self.count as f32;
-    }
-    // fn intervals(&self,  start: u128, end: u128) {}
 }
 
 fn main() {
