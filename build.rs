262c5f8df9225c76e5dc752166a9d4ae3e0572b7
John Shewchuk <johnshew@users.noreply.github.com>
2020-12-04 08:53:45 -0500

    add resources and manifest

--- build.rs
+++ build.rs
@@ -1,9 +1,5 @@
-extern crate winres;
-
+  
+extern crate embed_resource;
 fn main() {
-    if cfg!(target_os = "windows") {
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("resources/cog.ico");
-        res.compile().unwrap();
-    }
+    embed_resource::compile("embed_resources.rc");
 }
